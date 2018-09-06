32b666bbfd101a9362e6d05adea808a54b9b791c
Slobodan Vlaic <slobiiv@gmail.com>
2018-09-06 16:08:39 +0100

    My first comming 06/09/2018

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// // Smooth Scrolling
+// $('.cf a').on('click', function(event) {
+//   if (this.hash !== '') {
+//     event.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       800,
+//       function() {
+//         window.location.hash = hash;
+//       }
+//     );
+//   }
+// });
